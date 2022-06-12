88ce444f11ab4bbf326270990962a71c9dc09015
AnK <AnK@DESKTOP-35CEPU1>
2022-06-13 04:42:57 +0530

    day 7 code

---
+++ Day 7/src/com/ank/constructor/age.java
@@ -0,0 +1,13 @@
+package com.ank.constructor;
+class age {
+
+    int age;
+    age(int age){
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        age x = new age(23);
+        System.out.println("my age is = " + x.age);
+    }
+}
